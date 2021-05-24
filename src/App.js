52cb33c84a2a3004499b877092160ff762aa19b3
Eric Nguyen <erictn3@uci.edu>
2021-05-24 16:32:46 -0700

    start page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
+// import Logic from './components/Logic';
+// import Question from './components/Question/Question';
+// import Start from './components/Start/Start';
 
-function App() {
+import Start from './components/Start/Start';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Start/>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Question/Question.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Question = ({}) => {
+  return (
+    <div className="card">
+      <div className="card-content">
+
+      </div>
+    </div>
+  )
+}
+
+export default Question;
