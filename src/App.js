c7b0c406cabafb249a024d1ea0d717a95c69066b
Eric Nguyen <erictn3@uci.edu>
2021-05-24 18:04:45 -0700

    question component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-// import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 // import Logic from './components/Logic';
-// import Question from './components/Question/Question';
+import Question from './components/Question/Question';
 // import Start from './components/Start/Start';
+import quizData from './data/quiz.json';
 
 import Start from './components/Start/Start';
 
 const App = () => {
+  const [step, setStep] = useState(1);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+
+  const quizStartHandler = () => {
+    setStep(2);
+  }
+
   return (
     <div className="App">
-      <Start/>
-
+      {step ===1 && <Start onQuizStart={quizStartHandler} /> }
+      {step ===2 && <Question
+        data={quizData.data[activeQuestion]}
+        onAnswerUpdate={setAnswers}
+        numberOfQuestions={quizData.data.length}
+        activeQuestion={activeQuestion}
+        onSetActiveQuestion={setActiveQuestion}
+        onSetstep={setStep}
+      
+      />}
     </div>
   )
 }
--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,10 +1,52 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const Question = ({}) => {
+const Question = ({ data, onAnswerUpdate, numberOfQuestions, activeQuestion, onSetActiveQuestion, onSetstep}) => {
+
+  const [selected, setSelected] = useState('');
+  const [error, setError] = useState('');
+  const radiosWrapper = useRef();
+
+  useEffect(() => {
+    const findCheckedInput = radiosWrapper.current.querySelector('input:checked');
+    if(findCheckedInput) {
+      findCheckedInput.checked = false;
+    }
+  }, [data]);
+
+  const changeHandler = (e) => {
+    setSelected(e.target.value);  
+  }
+
+  const nextClickHandler = (e) => {
+    if(selected === '') {
+      return setError('Please select an option');
+    }
+    onAnswerUpdate(prevState => [...prevState, { q: data.question, a: selected }]);
+    setSelected('');
+    if(activeQuestion < numberOfQuestions -1) {
+      onSetActiveQuestion(activeQuestion +1);
+    } else {
+      onSetstep(3);
+    }
+  }
+
   return (
     <div className="card">
       <div className="card-content">
-
+        <div className="content">
+          <h2 className="mb-5">{data.title}</h2>
+          <div className="control" ref={radiosWrapper}>
+            {data.choices.map((choice, i) => (
+              <label className="radio has-background-light">
+              <input type="radio" name="answer" value={choice} onChange={changeHandler}>
+              </input>
+              {choice}
+            </label>
+            ))} 
+            {error && <div className="has-text-danger">{error}</div>}
+            <button className="button is-link is-medium is-fullwidth mt-4" onClick={nextClickHandler}>Next</button>
+          </div>
+        </div>
       </div>
     </div>
   )
